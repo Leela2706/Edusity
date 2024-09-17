0aaefb1d35eadb9ad490d6875955a157515ccfbc
leela2706 <saileelavardhan12@gmail.com>
2024-09-17 15:57:56 +0530

    Project Edusity

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
@@ -7,8 +7,13 @@ import About from './Components/About/about'
 import Campus from './Components/Campus/Campus'
 import Testimonials from './Components/Testmonials/Testimonials'
 import Contact from './Components/Contact/contact'
+import Footer from './Components/Footer/Footer'
+import VideoPlayer from './Components/VideoPlayer/VideoPlayer'
 
 const App = () => {
+
+  const [playState, setPlayState] = useState(false);
+
   return (
     <div>
       <Navbar/>
@@ -16,15 +21,16 @@ const App = () => {
       <div className="container">
         <Title subTitle='Our Program' title='What We Offer'/>
       <Programs/>
-      <About/>
+      <About setPlayState={setPlayState}/>
       <Title subTitle='Gallery' title='Campus Photos'/>
       <Campus/>
       <Title subTitle='TESTIMONIALS' title='What Student Says'/>
       <Testimonials/>
       <Title subTitle='Contact Us' title='Get in Touch'/>
       <Contact/>
+      <Footer/>
       </div>
-
+      <VideoPlayer playState={playState} setPlayState={setPlayState}/>
     </div>
   )
 }
--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -7,6 +7,33 @@ import location_icon from '../../assets/location-icon.png'
 import white_arrow from '../../assets/white-arrow.png'
 
 const Contact = () => {
+
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "c54b1e0c-e392-4164-b2b6-4437ee4de0bd");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+
   return (
     <div className='contact'>
       <div className="contact-col">
@@ -19,7 +46,7 @@ const Contact = () => {
         </ul>
       </div>
       <div className="contact-col">
-        <form>
+        <form onSubmit={onSubmit}>
             <label> Your Name</label>
             <input type="text" name="name" placeholder="Enter Your Name" required/>
             <label>Phone Number</label>
@@ -28,7 +55,7 @@ const Contact = () => {
             <textarea name="message" rows="6" placeholder='Enter Your Text' required></textarea>
             <button type='submit' className='btn dark-btn'>Submit Now <img src={white_arrow} alt="" /></button>
         </form>
-        <span></span>
+        <span>{result}</span>
       </div>
     </div>
   )
